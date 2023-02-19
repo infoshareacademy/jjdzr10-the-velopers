4f61cad7fafbb3a64ac291e1ccefc8cfbdb8698b
KarolBilas <karolbilas@wp.pl>
2023-02-19 12:43:05 +0100

    Added exceptions for incorrect menu options

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -6,8 +6,7 @@ import com.infoshareacademy.service.UserService;
 
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception{
         System.out.println( "The Velopers" );
         User currentUser = UserService.createUser();
         MenuSelection.mainMenu();
--- src/main/java/com/infoshareacademy/service/MenuSelection.java
+++ src/main/java/com/infoshareacademy/service/MenuSelection.java
@@ -4,32 +4,39 @@ import java.util.Scanner;
 
 public class MenuSelection {
 
-    public static void mainMenu(){
+    public static void mainMenu() {
         int menuOption;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Wybierz opcje:");
         System.out.println("1 - nowa gra");
         System.out.println("2 - zarządzaj pytaniami");
         System.out.println("3 - wyjście");
-        menuOption = scanner.nextInt();
-        //todo walidacja i wyjatki
-        switch(menuOption){
-            case 1:
-                //nowa gra
-                break;
-            case 2:
-                //zarządzaj pytaniami
-                editMenu();
-                break;
-            case 3:
-                //wyjście
-                System.out.println("Dzięki za wspólną zabawę!");
-                break;
-            default:
+
+        try {
+            menuOption = scanner.nextInt();
+            //todo walidacja i wyjatki
+            switch (menuOption) {
+                case 1:
+                    //nowa gra
+                    break;
+                case 2:
+                    //zarządzaj pytaniami
+                    editMenu();
+                    break;
+                case 3:
+                    //wyjście
+                    System.out.println("Dzięki za wspólną zabawę!");
+                    break;
+                default:
+            }
+        } catch (Exception e) {
+            System.out.println("Nieprawidłowa opcja!");
+            mainMenu();
         }
     }
 
-    public static void editMenu(){
+
+    public static void editMenu() {
         int menuOption;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Wybierz opcje:");
@@ -37,23 +44,28 @@ public class MenuSelection {
         System.out.println("2 - usuń pytanie");
         System.out.println("3 - edytuj pytanie");
         System.out.println("4 - powrót do menu");
-        menuOption = scanner.nextInt();
-        //todo walidacja i wyjatki
-        switch(menuOption){
-            case 1:
-                //dodaj pytanie
-                break;
-            case 2:
-                //usuń pytanie
-                break;
-            case 3:
-                //edytuj pytanie
-                break;
-            case 4:
-                //wróć do menu głównego
-                mainMenu();
-                break;
-            default:
+        try {
+            menuOption = scanner.nextInt();
+            //todo walidacja i wyjatki
+            switch (menuOption) {
+                case 1:
+                    //dodaj pytanie
+                    break;
+                case 2:
+                    //usuń pytanie
+                    break;
+                case 3:
+                    //edytuj pytanie
+                    break;
+                case 4:
+                    //wróć do menu głównego
+                    mainMenu();
+                    break;
+                default:
+            }
+        } catch (Exception e) {
+            System.out.println("Nieprawidłowa opcja!");
+            mainMenu();
         }
     }
 
