fb6716a7db7c1bcd353a6ff6fc5e239ebf60151f
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-02-26 13:27:32 +0100

    add getAnswer to QuestionService

--- src/main/java/com/infoshareacademy/service/QuestionService.java
+++ src/main/java/com/infoshareacademy/service/QuestionService.java
@@ -63,6 +63,45 @@ public class QuestionService {
         answers[3] = userAnswer4;
         return answers;
     }
+    private static boolean[] getCorrectAnswer() {
+        Scanner scanner = new Scanner(System.in);
+        boolean userAnswerBoolean[] = new boolean[4];
+
+        System.out.print("Jaka jest poprawna odpowiedź?: ");
+        boolean isCorecct = false;
+        while (!isCorecct) {
+            String userAnswers = scanner.nextLine().toUpperCase();
+            // TODO zabezpieczy przed podaniem odpowiedzi w postaci: "A,Z" . Gdy jedna litera jest A,B,C,D i podajemy jeszcze inna z poza tego zakresu
+            if (userAnswers.contains("A") || userAnswers.contains("B") || userAnswers.contains("C") || userAnswers.contains("D")) {
+                userAnswerBoolean = convertAnswer(userAnswers);
+                isCorecct = true;
+            } else {
+                System.out.print("Podaj jeszcze raz odpowiedź z zakresu: A,B,C,D :");
+            }
+        }
+        return userAnswerBoolean;
+    }
+
+    private static boolean[] convertAnswer(String userAnswer){
+        String[] splitUserAnswers = userAnswer.split(",",4);
+        boolean[] booleansAnswers = {false,false,false,false};
+        for (int i=0;i<splitUserAnswers.length;i++) {
+            switch (splitUserAnswers[i]) {
+                case "A":
+                    booleansAnswers[0] = true;
+                    break;
+                case "B":
+                    booleansAnswers[1] = true;
+                    break;
+                case "C":
+                    booleansAnswers[2] = true;
+                    break;
+                case "D":
+                    booleansAnswers[3] = true;
+                    break;
+            }
+        } return booleansAnswers;
+    }
 }
 
 
