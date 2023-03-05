e54f5ef5f19308c75692141f7402cac57b04ae82
KarolBilas <karolbilas@wp.pl>
2023-03-05 14:02:51 +0100

    Created spring project

---
+++ web-app/src/test/java/com/infoshare/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshare.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
