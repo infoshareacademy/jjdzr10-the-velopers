e671be46dbae4e3e8a3eeba8bf7947113c606795
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-03-06 20:49:31 +0100

    transfer model class

---
+++ web-app/src/main/java/com/infoshare/webapp/model/Category.java
@@ -0,0 +1,7 @@
+package com.infoshare.webapp.model;
+
+public enum Category {
+    SZTUKA,
+    BIOLOGIA,
+    HISTORIA,
+}
---
+++ web-app/src/main/java/com/infoshare/webapp/model/Questions.java
@@ -0,0 +1,62 @@
+package com.infoshare.webapp.model;
+
+public class Questions {
+    private int idQuestion;
+
+    private Category category;
+    private Answers answer;
+    private String questionText;
+    private int score;
+
+    public int getIdQuestion() {
+        return idQuestion;
+    }
+
+    public void setIdQuestion(int idQuestion) {
+        this.idQuestion = idQuestion;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Answers getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answers answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Questions{" +
+                "idQuestion=" + idQuestion +
+                ", category='" + category + '\'' +
+                ", answer=" + answer +
+                ", questionText='" + questionText + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
+
