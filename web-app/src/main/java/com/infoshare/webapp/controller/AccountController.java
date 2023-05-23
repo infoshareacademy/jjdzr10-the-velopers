bba03b086d5375e0252b9c55e40e78498734c5c3
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-05-23 15:03:28 +0200

    add registration feature

--- web-app/src/main/java/com/infoshare/webapp/controller/AccountController.java
+++ web-app/src/main/java/com/infoshare/webapp/controller/AccountController.java
@@ -1,40 +1,51 @@
 package com.infoshare.webapp.controller;
 
-import com.infoshare.webapp.model.User;
-import com.infoshare.webapp.repository.UserRepository;
+import com.infoshare.webapp.Dto.UserDto;
+import com.infoshare.webapp.exception.UserAlreadyExistException;
+import com.infoshare.webapp.service.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private static final Logger LOGGER = LogManager.getLogger(AccountController.class);
 
-    public AccountController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public AccountController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/reset_pass")
     public String resetPass(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
         return "reset_password";
     }
 
     @PostMapping("/register")
-    public String submitForm(@ModelAttribute("user") User user) {
-        LOGGER.info("Register new user: (id:{}) {}", user.getId(), user.getUsername());
-        userRepository.save(user);
+    public String registrationUserAccount(@ModelAttribute("user") @Valid UserDto userDto, RedirectAttributes redirectAttributes) {
+        try {
+            userService.registerNewUserAccount(userDto);
+            LOGGER.info("Register new user: {}", userDto.getUsername());
+        }
+            catch (UserAlreadyExistException e){
+                redirectAttributes.addFlashAttribute("message", "Used e-mail already exist!");
+                redirectAttributes.addFlashAttribute("messageType","danger");
+                return "redirect:/register";
+            }
         return "index";
     }
 
     @GetMapping("/register")
-    public String showForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
+    public String showRegisterForm(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
         return "register_form";
     }
 
--- web-app/src/main/java/com/infoshare/webapp/service/UserService.java
+++ web-app/src/main/java/com/infoshare/webapp/service/UserService.java
@@ -1,26 +1,44 @@
 package com.infoshare.webapp.service;
 
+import com.infoshare.webapp.Dto.UserDto;
+import com.infoshare.webapp.exception.UserAlreadyExistException;
+import com.infoshare.webapp.model.Role;
 import com.infoshare.webapp.model.User;
+import com.infoshare.webapp.repository.RoleRepository;
 import com.infoshare.webapp.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
 public class UserService {
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-
-    public User saveUser(User user){
+    public User registerNewUserAccount(UserDto userDto) throws UserAlreadyExistException {
+        if (emailExists(userDto.getEmail())) {
+            throw new UserAlreadyExistException("There is an account with that email address: "
+                    + userDto.getEmail());
+        }
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setEmail(userDto.getEmail());
+        Role role = roleRepository.findByName("USER");
+        user.setRoles(Set.of(role));
+        role.getUsers().add(user);
         return userRepository.save(user);
     }
-
-    public List<User> getAll(Long id){
-        return userRepository.findAll();
+    private boolean emailExists(String email) {
+        return userRepository.findUserByEmail(email).isPresent();
     }
 }
