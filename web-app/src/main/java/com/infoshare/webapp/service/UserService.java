ae811291e4af82d13ca46dd75daca58b214e70a6
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-05-23 13:01:32 +0200

    change UserService

--- web-app/src/main/java/com/infoshare/webapp/service/UserService.java
+++ web-app/src/main/java/com/infoshare/webapp/service/UserService.java
@@ -1,23 +1,26 @@
 package com.infoshare.webapp.service;
 
 import com.infoshare.webapp.model.User;
+import com.infoshare.webapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
-import java.util.Scanner;
+import javax.transaction.Transactional;
+import java.util.List;
 
+@Service
+@Transactional
 public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    private static Scanner scanner;
-    public static User createUser() {
-        scanner = new Scanner(System.in);
-        User newUser;
-        while (true) {
-            System.out.println("Podaj nazwę użytkownika");
-            String userName = scanner.nextLine();
-            if (userName.length() > 0) {
-                newUser = new User();
-                newUser.setUserName(userName);
-                return newUser;
-            }
-        }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAll(Long id){
+        return userRepository.findAll();
     }
 }
