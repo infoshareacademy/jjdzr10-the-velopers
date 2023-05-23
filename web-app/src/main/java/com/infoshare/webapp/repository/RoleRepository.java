4718836ac5b33378804a143234c777fb2af41424
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-05-23 13:01:01 +0200

    add UserRepository, RoleRepository

---
+++ web-app/src/main/java/com/infoshare/webapp/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.infoshare.webapp.repository;
+
+import com.infoshare.webapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
