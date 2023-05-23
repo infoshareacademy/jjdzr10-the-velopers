2d68e7b7a8282bd733dc2107d08ff270425842b3
M-Jakowczyk <mateusz.jakowczyk@o2.pl>
2023-05-23 15:02:58 +0200

    small fix User model

--- web-app/src/main/java/com/infoshare/webapp/model/User.java
+++ web-app/src/main/java/com/infoshare/webapp/model/User.java
@@ -16,7 +16,7 @@ import java.util.*;
 @Setter
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "name", nullable = false)
